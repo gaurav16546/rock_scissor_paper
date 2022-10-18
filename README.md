5c59f8b61d6b8db473a12dbaf9cf71f2475ac264
Gaurav Thapa <gaurav16547@gmail.com>
2022-10-18 20:45:10 +0545

    playround function is complete. this helps to play the actual game

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # rock_scissor_paper
 Third project of The odin project
+In the third project of The odin project i will make playlable rock scissor paper with the help of javascript only and nothing else.
+this game will be playable in the console only and nothing else.
+I will use what i have learned till now such as function, conditional statement and loop.
--- javascript.js
+++ javascript.js
@@ -8,10 +8,71 @@ function getComputeChoice (randomNumber)
     {
         return "paper";
     }
-    else return "rock";
+    else
+    {
+    return "rock";
+    }
 }
 
 randomNumber = Math.floor(Math.random() * 10 );
 const computerSelection = getComputeChoice(randomNumber);
-// const yourSelection = prompt("Enter your choice");
-console.log(computerSelection)
+let yourSelection = prompt("Enter your choice");
+yourSelection = yourSelection.toLowerCase();
+// console.log(yourSelection);
+
+function playRound(computerSelection,yourSelection)
+{
+    if(yourSelection === "rock")
+    {
+        if(computerSelection === "rock")
+        {
+            return "draw";
+        }
+
+        else if(computerSelection === "paper")
+        {
+            return "lose"
+        }
+        else 
+        {
+            return "win";
+        }
+    }
+    else if(yourSelection === "scissor")
+    {
+        if(computerSelection === "scissor")
+        {
+            return "draw";
+        }
+
+        else if(computerSelection === "rock")
+        {
+            return "lose"
+        }
+        else 
+        {
+            return "win";
+        }
+    }
+    else 
+    {
+    
+        if(computerSelection === "paper")
+        {
+            return "draw";
+        }
+
+        else if(computerSelection === "scissor")
+        {
+            return "lose"
+        }
+        else 
+        {
+            return "win";
+        }
+    
+    }
+}
+console.log(`Your choice is ${yourSelection}`);
+console.log(`Computer choice is ${computerSelection}`);
+console.log(playRound(computerSelection,yourSelection));
