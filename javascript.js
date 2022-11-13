de21803770db94d87c7dcc6debc7abd5a15aa7c6
Gaurav Thapa <gaurav16547@gmail.com>
2022-11-13 14:40:06 +0545

    can play single round

--- javascript.js
+++ javascript.js
@@ -1,3 +1,9 @@
+//getting nodes from the html file
+const rock = document.getElementById('rock');
+const paper = document.getElementById('paper');
+const scissor = document.getElementById('scissor');
+const div = document.querySelector('#result');
+
 //function to randomly return computer choice
 function getComputeChoice(randomNumber) {
     if (randomNumber === 1 || randomNumber === 6 || randomNumber === 9) {
@@ -11,13 +17,11 @@ function getComputeChoice(randomNumber) {
     }
 }
 
-// function bothSelection
-
-// console.log(yourSelection);
-
-
 //actually play game 
 function playRound(computerSelection, yourSelection) {
+
+    // console.log(`Your choice is ${yourSelection}
+    // and computer choice is ${computerSelection}`)
     if (yourSelection === "rock") {
         if (computerSelection === "rock") {
             return "draw";
@@ -57,47 +61,72 @@ function playRound(computerSelection, yourSelection) {
 
     }
 }
-// console.log(`Your choice is ${yourSelection}`);
-// console.log(`Computer choice is ${computerSelection}`);
-// console.log(playRound(computerSelection, yourSelection));
 
-let win = 0, 
-lose = 0;
+//function to play a single round
+function roundResult(loopGame){
+    if(loopGame === 'draw'){
+       let draw = document.createElement('p');
+       draw.textContent = "Game is draw";
+        div.appendChild(draw);
+        return;
+    }
+    else if(loopGame === 'win'){
+        let win = document.createElement('p');
+        win.textContent = "You win";
+        div.appendChild(win);
+        return;
+    }
+    else{
+        let lose = document.createElement('p');
+        lose.textContent = "You lose";
+        div.appendChild(lose);
+        return;
+    }
+}
+
+
+// function overallResult(win,lose){
+//     if(win > lose)
+//     {
+//         console.log("You win the game.");
+//     }
+//     else if(lose > win )
+//     console.log("You lose the game.");
+//     else 
+//     console.log("Game draw.");
+// }
+
+
+
 //function to play multiple round
-for (let i = 0; i < 5; i++) {
-
-
-
-    randomNumber = Math.floor(Math.random() * 10);
-    let computerSelection = getComputeChoice(randomNumber);
-    let yourSelection = prompt("Enter your choice");
-    yourSelection = yourSelection.toLowerCase();
-    let roundResult = playRound(computerSelection, yourSelection);
+// for (let i = 0; i < 5; i++) {
+    let loopGame = "";
+    let randomNumber = Math.floor(Math.random() * 10);
+    let computerSelection = getComputeChoice(randomNumber)
     
+    rock.addEventListener('click',() => {
+    
+        yourSelection = 'rock';
+        loopGame = playRound(computerSelection,yourSelection);
+        roundResult(loopGame);
+    });
+    
+    paper.addEventListener('click',() => {
+    
+        yourSelection = 'paper';
+        loopGame = playRound(computerSelection,yourSelection);
+        roundResult(loopGame);
+    });
+    
+    scissor.addEventListener('click',() => {
+    
+        yourSelection = 'scissor';
+        loopGame = playRound(computerSelection,yourSelection);
+        roundResult(loopGame);
+    });
+// }
 
 
-    if (roundResult === "draw") {
-        console.log(`Game draw! you both chose ${yourSelection}`);
-    }
-    else if (roundResult === "win") {
-        console.log(`You win! ${yourSelection} beats ${computerSelection}`);
-        win+=1;
-    }
-    else {
-        lose+=1;
-        console.log(`You lose! ${computerSelection} beats ${yourSelection}`);
-    }
-}
 
-overallResult(win,lose);
+// overallResult(win,lose);
 
-function overallResult(win,lose){
-    if(win > lose)
-    {
-        console.log("You win the game.");
-    }
-    else if(lose > win )
-    console.log("You lose the game.");
-    else 
-    console.log("Game draw.");
-}
