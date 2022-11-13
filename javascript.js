b3ec1ba40a298c99b89be46ede71d3024143884d
Gaurav Thapa <gaurav16547@gmail.com>
2022-11-13 19:57:41 +0545

    added restart button

--- javascript.js
+++ javascript.js
@@ -1,132 +1,104 @@
-//getting nodes from the html file
-const rock = document.getElementById('rock');
-const paper = document.getElementById('paper');
-const scissor = document.getElementById('scissor');
-const div = document.querySelector('#result');
+const game = () => {
+    let yourScore = 0,
+        computerScore = 0;
 
-//function to randomly return computer choice
-function getComputeChoice(randomNumber) {
-    if (randomNumber === 1 || randomNumber === 6 || randomNumber === 9) {
-        return "scissor";
+    const playGame = () => {
+
+        const rockbtn = document.getElementById('rock');
+        const paperbtn = document.getElementById('paper');
+        const scissorbtn = document.getElementById('scissor');
+        const yourchoice = [rockbtn, scissorbtn, paperbtn];
+        const computerchoice = ['rock', 'scissor', 'paper'];
+
+
+        yourchoice.forEach(option => {
+            option.addEventListener('click', function () {
+                const randomNumber = Math.floor(Math.random() * 3);
+                const computerSelection = computerchoice[randomNumber];
+
+                roundResult(computerSelection, this.innerText);
+
+                if (yourScore === 5 || computerScore === 5) {
+                    gameOver(yourchoice);
+                }
+            })
+        });
     }
-    else if (randomNumber === 2 || randomNumber === 5 || randomNumber === 8) {
-        return "paper";
+
+    playGame();
+
+    function roundResult(computerSelection, yourSelection) {
+        yourSelection = yourSelection.toLowerCase();
+        const div = document.querySelector('.result');
+
+
+        if (yourSelection === "rock") {
+            if (computerSelection === "rock") {
+                div.textContent = "Draw";
+            }
+
+            else if (computerSelection === "paper") {
+                div.textContent = "lose";
+                computerScore++;
+            }
+            else {
+                div.textContent = "win";
+                yourScore++;
+            }
+        }
+        else if (yourSelection === "scissor") {
+            if (computerSelection === "scissor") {
+                div.textContent = "Draw";
+            }
+
+            else if (computerSelection === "rock") {
+                div.textContent = "lose";
+                computerScore++;
+            }
+
+            else {
+                div.textContent = "win";
+                yourScore++;
+            }
+        }
+        else {
+
+            if (computerSelection === "paper") {
+                div.textContent = "Draw";
+            }
+
+            else if (computerSelection === "scissor") {
+                div.textContent = "lose";
+                computerScore++;
+            }
+            else {
+                div.textContent = "win";
+                yourScore++;
+            }
+
+        }
     }
-    else {
-        return "rock";
+
+    const gameOver = (yourchoice,) => {
+       
+       const restartBtn = document.querySelector('.restart');
+        const overall = document.querySelector('.overall-result');
+        yourchoice.forEach(option => {
+            option.style.display = 'none';
+        });
+
+
+        if (yourScore === 5) {
+            overall.textContent = "You won."
+        }
+        else {
+            overall.textContent = "You lost. Better luck next time!";
+        }
+        restartBtn.addEventListener('click', () =>{
+            window.location.reload();
+        })
     }
 }
 
-//actually play game 
-function playRound(computerSelection, yourSelection) {
-
-    // console.log(`Your choice is ${yourSelection}
-    // and computer choice is ${computerSelection}`)
-    if (yourSelection === "rock") {
-        if (computerSelection === "rock") {
-            return "draw";
-        }
-
-        else if (computerSelection === "paper") {
-            return "lose";
-        }
-        else {
-            return "win";
-        }
-    }
-    else if (yourSelection === "scissor") {
-        if (computerSelection === "scissor") {
-            return "draw";
-        }
-
-        else if (computerSelection === "rock") {
-            return "lose";
-        }
-        else {
-            return "win";
-        }
-    }
-    else {
-
-        if (computerSelection === "paper") {
-            return "draw";
-        }
-
-        else if (computerSelection === "scissor") {
-            return "lose";
-        }
-        else {
-            return "win";
-        }
-
-    }
-}
-
-//function to play a single round
-function roundResult(loopGame){
-    if(loopGame === 'draw'){
-       let draw = document.createElement('p');
-       draw.textContent = "Game is draw";
-        div.appendChild(draw);
-        return;
-    }
-    else if(loopGame === 'win'){
-        let win = document.createElement('p');
-        win.textContent = "You win";
-        div.appendChild(win);
-        return;
-    }
-    else{
-        let lose = document.createElement('p');
-        lose.textContent = "You lose";
-        div.appendChild(lose);
-        return;
-    }
-}
-
-
-// function overallResult(win,lose){
-//     if(win > lose)
-//     {
-//         console.log("You win the game.");
-//     }
-//     else if(lose > win )
-//     console.log("You lose the game.");
-//     else 
-//     console.log("Game draw.");
-// }
-
-
-
-//function to play multiple round
-// for (let i = 0; i < 5; i++) {
-    let loopGame = "";
-    let randomNumber = Math.floor(Math.random() * 10);
-    let computerSelection = getComputeChoice(randomNumber)
-    
-    rock.addEventListener('click',() => {
-    
-        yourSelection = 'rock';
-        loopGame = playRound(computerSelection,yourSelection);
-        roundResult(loopGame);
-    });
-    
-    paper.addEventListener('click',() => {
-    
-        yourSelection = 'paper';
-        loopGame = playRound(computerSelection,yourSelection);
-        roundResult(loopGame);
-    });
-    
-    scissor.addEventListener('click',() => {
-    
-        yourSelection = 'scissor';
-        loopGame = playRound(computerSelection,yourSelection);
-        roundResult(loopGame);
-    });
-// }
-
-
-
-// overallResult(win,lose);
+game();
 
